b17bbff0fdcdf576dd0478cee1b7a976024341b9
Lynextion <47065577+Lynextion@users.noreply.github.com>
2020-11-22 17:58:37 +0300

    again fix something

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^1sy52qg-@(!0smyb%=#femj+ik_vq&%g)ey*nx-2j3(w-ksxj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
