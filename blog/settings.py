efbc502f33f943499265c4897736fbdb17f32495
Lynextion <47065577+Lynextion@users.noreply.github.com>
2020-11-23 19:19:24 +0300

    h

--- blog/settings.py
+++ blog/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'assets'),
 
